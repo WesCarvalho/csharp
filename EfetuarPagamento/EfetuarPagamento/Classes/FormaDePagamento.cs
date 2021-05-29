5c20b5445e6f71b82e83aee9a084e8d0b4c9ed8a
WesCarvalho <weslley_lopes@hotmail.com>
2021-05-29 11:36:47 -0300

    Exemplo Polimorfismo

---
+++ EfetuarPagamento/EfetuarPagamento/Classes/FormaDePagamento.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfetuarPagamento.Classes
+{
+    class FormaDePagamento
+    {
+        public virtual void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado");
+        }
+    }
+
+    class FormaDePagamentoBoleto : FormaDePagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado com boleto");
+        }
+    }
+
+    class FormaDePagamentoPix : FormaDePagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado com Pix");
+        }
+    }
+
+    class FormaDePagamentoCartaoCredito : FormaDePagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado com Cartão de Crédito");
+        }
+    }
+
+    class FormaDePagamentoTransferencia : FormaDePagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado por Transferencia ");
+        }
+    }
+}
---
+++ EfetuarPagamento/EfetuarPagamento/Program.cs
@@ -0,0 +1,56 @@
+﻿using EfetuarPagamento.Classes;
+using EfetuarPagamento.Enums;
+using System;
+
+namespace EfetuarPagamento
+{
+    class Program
+    {
+        static void Main(string[] produtos)
+        {
+          if(produtos.Length ==0)
+          {
+                Console.WriteLine("Nenhum produto foi listado para compra.");
+                return;
+          }
+
+            Console.WriteLine("Favor informar a forma de pagamento (Boleto, Pix, Cartão de Credito, Transferencia)");
+            var formaPagamentoDesejada = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(formaPagamentoDesejada) || string.IsNullOrWhiteSpace(formaPagamentoDesejada)
+                || (TipoPagamentoEnum.Boleto.ToString() != formaPagamentoDesejada
+                    && TipoPagamentoEnum.Pix.ToString() != formaPagamentoDesejada
+                    && TipoPagamentoEnum.CartaoCredito.ToString() != formaPagamentoDesejada
+                    && TipoPagamentoEnum.Transferencia.ToString() != formaPagamentoDesejada))
+            {
+                Console.Write($"A forma de pagamento: {formaPagamentoDesejada} não é válida!");
+                return;
+            }
+
+            FormaDePagamento formaDePagamento;
+            if(TipoPagamentoEnum.Boleto.ToString() == formaPagamentoDesejada)
+            {
+                formaDePagamento = new FormaDePagamentoBoleto();
+            }else if (TipoPagamentoEnum.Pix.ToString() == formaPagamentoDesejada)
+            {
+                formaDePagamento = new FormaDePagamentoPix();
+            }else if (TipoPagamentoEnum.CartaoCredito.ToString() == formaPagamentoDesejada)
+            {
+                formaDePagamento = new FormaDePagamentoCartaoCredito();
+            }else if (TipoPagamentoEnum.Transferencia.ToString() == formaPagamentoDesejada)
+            {
+                formaDePagamento = new FormaDePagamentoTransferencia();
+            }else
+            {
+                Console.WriteLine($"A forma de pagamento: {formaPagamentoDesejada} não é válida!");
+                return;
+            }
+
+            if (formaDePagamento != null)
+            {
+                formaDePagamento.EfetuarPagamento();
+            }
+        }
+
+    }
+}
